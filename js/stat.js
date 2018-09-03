3fce79d75e84e8fb3f2cd81294cefffd4e19ee39
Cidojo <cidell@mail.ru>
2018-09-03 18:22:12 +0300

    done, but scale problem

--- js/stat.js
+++ js/stat.js
@@ -6,10 +6,11 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var SHADOW_GAP = 10;
 var GAP = 20;
+var BAR_GAP = 50;
 var FONT_SIZE = 16;
-var FONT_GAP = 12;
+var FONT_GAP = 8;
 var BAR_WIDTH = 40;
-var barHeight = CLOUD_HEIGHT - GAP*2 - FONT_SIZE*4 + FONT_GAP*2;
+var barHeight = CLOUD_HEIGHT - GAP*2 - FONT_SIZE*4 - FONT_GAP*2;
 
 
 function renderCloud(ctx, x, y, color) {
@@ -18,12 +19,12 @@ function renderCloud(ctx, x, y, color) {
 }
 
 function getMaxTime(array) {
+  var maxTime = array[0];
   for (var i = 1; i < array.length; i++) {
-    var maxTime = array[0];
     if (array[i] > maxTime) {
       maxTime = array[i];
     }
-    return maxTime;
+    return Math.round(maxTime);
   }
 }
 
@@ -36,10 +37,17 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, GAP + FONT_SIZE);
   ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP + FONT_SIZE*2);
 
-  // var scaleMultiplier = maxTime/150;
+  var scaleMultiplier = barHeight/getMaxTime(times);
 
-  ctx.fillText(Math.round(getMaxTime(times)), CLOUD_X + GAP, GAP + FONT_SIZE*3 + FONT_GAP);
-  ctx.fillRect(CLOUD_X + GAP, GAP + (FONT_SIZE)*3 + FONT_GAP*2, BAR_WIDTH, barHeight);
-  ctx.fillText(names[0], CLOUD_X + GAP, CLOUD_Y + CLOUD_HEIGHT - GAP);
-
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      var randomSaturay = Math.round(Math.random())*100;
+      ctx.fillStyle = `hsl(240, ${randomSaturay}%, 50%)`;
+    }
+    ctx.fillText(names[i], CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP)*i, CLOUD_Y + CLOUD_HEIGHT - GAP);
+    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP)*i, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_SIZE -FONT_GAP, BAR_WIDTH, (-1)*Math.round(times[i])*scaleMultiplier);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP)*i, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_SIZE - FONT_GAP*2 - Math.round(times[i])*scaleMultiplier);
+  }
 };
