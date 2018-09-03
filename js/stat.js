85655c1d8896ffdb8c18de13cce9b218459434be
Cidojo <cidell@mail.ru>
2018-09-03 17:30:09 +0300

    just a save

--- js/stat.js
+++ js/stat.js
@@ -4,10 +4,12 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var TEXT_HEIGHT = 16;
-var GAP = 60;
+var SHADOW_GAP = 10;
+var GAP = 20;
+var FONT_SIZE = 16;
+var FONT_GAP = 12;
 var BAR_WIDTH = 40;
-var BAR_HEIGHT = CLOUD_HEIGHT - GAP - TEXT_HEIGHT - GAP;
+var barHeight = CLOUD_HEIGHT - GAP*2 - FONT_SIZE*4 + FONT_GAP*2;
 
 
 function renderCloud(ctx, x, y, color) {
@@ -15,25 +17,29 @@ function renderCloud(ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 }
 
+function getMaxTime(array) {
+  for (var i = 1; i < array.length; i++) {
+    var maxTime = array[0];
+    if (array[i] > maxTime) {
+      maxTime = array[i];
+    }
+    return maxTime;
+  }
+}
+
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
   ctx.fillStyle = 'black';
-  ctx.font = `${TEXT_HEIGHT} \'PT Mono\'`;
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP + TEXT_HEIGHT);
+  ctx.font = `${FONT_SIZE} \'PT Mono\'`;
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, GAP + FONT_SIZE);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP + FONT_SIZE*2);
 
-  for (var i = 1; i < times.length; i++) {
-    var maxTime = times[0];
-    if (times[i] > maxTime) {
-      maxTime = times[i];
-    }
-  }
+  // var scaleMultiplier = maxTime/150;
 
-  var scaleMultiplier = maxTime/150;
+  ctx.fillText(Math.round(getMaxTime(times)), CLOUD_X + GAP, GAP + FONT_SIZE*3 + FONT_GAP);
+  ctx.fillRect(CLOUD_X + GAP, GAP + (FONT_SIZE)*3 + FONT_GAP*2, BAR_WIDTH, barHeight);
+  ctx.fillText(names[0], CLOUD_X + GAP, CLOUD_Y + CLOUD_HEIGHT - GAP);
 
-  ctx.fillRect(170, 250, -40, -150);
-
-  ctx.fillText(Math.round(maxTime), 130, 270);
 };
